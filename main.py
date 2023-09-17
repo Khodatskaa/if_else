b45909c9a2d96301858c720beda726169798abaa
Khodatskaa <khodatskaalina@gmail.com>
2023-09-17 20:15:03 +0300

    Ex 3 was done

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 x = int(input(''))
-if :
-    print('')
+
+print('choose an option')
+print('1. miles')
+print('2. inches')
+print('3. yards')
+choice = input('paste a number of operation(1/2/3): ')
+
+if choice == '1':
+    print(x * 0.000621)
+elif choice == '2':
+    print(x * 39.37)
+elif choice == '3':
+    print(x * 1.093)
 else :
-    print('')
+    print('Unknown operation number')
