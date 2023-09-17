4b0187b8d4a6aaee0b96f53ba198039423255ace
Khodatskaa <khodatskaalina@gmail.com>
2023-09-17 18:38:28 +0300

    Ex 1 was done

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-x = int(input(''))
-if :
-    print('')
-else :
-    print('')
+x = float(input(''))
+y = float(input(''))
+z = float(input(''))
+
+print('choose an operation:')
+print('1. сума')
+print('2. добуток')
+choice = input('paste a number of operation(1/2):')
+
+if choice == '1':
+    print(x + y + z)
+elif choice == '2':
+    print(x * y * z)
+else:
+    print('Unknown operation number')
+
