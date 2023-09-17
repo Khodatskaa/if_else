4251a98b6790485e68c57728a2e2afa64aaacdef
Khodatskaa <khodatskaalina@gmail.com>
2023-09-17 20:00:12 +0300

    Ex 2 was done

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
-x = int(input(''))
-if :
-    print('')
-else :
-    print('')
+x = float(input(''))
+y = float(input(''))
+z = float(input(''))
+
+print("choose an operation:")
+print("1. max")
+print("2. min")
+print("3. середньоарифметичне")
+choice = input("paste a number of operation(1/2/3): ")
+
+if choice == "1":
+    result = max(x, y, z)
+    print(result)
+elif choice == "2":
+    result = min(x, y, z)
+    print(result)
+elif choice == "3":
+    print((x + y + z) / 3)
+else:
+    print("Unknown operation number")
